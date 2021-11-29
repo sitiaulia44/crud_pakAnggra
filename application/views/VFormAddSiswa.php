b62fac730e50696675f4bbf9a3f301a60705853b
sitiaulia44 <salyaaulia78@gmail.com>
2021-11-29 14:40:25 +0700

    pengumpulan tugas pak anggra

---
+++ application/views/VFormAddSiswa.php
@@ -0,0 +1,7 @@
+<form action="<?php echo site_url('Welcome/AddDataSiswa'); ?>" method="post">
+	NIS<input type="text" name="nis"><br>
+	Nama<input type="text" name="nama"><br>
+	Alamat<input type="text" name="alamat"><br>
+	
+	<input type="submit" name="simpan" value="Simpan">
+</form>
